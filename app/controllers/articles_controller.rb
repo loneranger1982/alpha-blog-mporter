e8b15ac52f066f67a6c6b9d778af5f03a241e04c
Martin Porter <Admin@shenobis.co.uk>
2017-10-10 17:22:43 +0000

    Flash message created show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,19 @@ def new
 end
 
 def create
-  render plain: params[:article].inspect
+  #render plain: params[:article].inspect
   @article = Article.new(article_params)
-  @article.save
+  if @article.save
+    flash[:notice] = "Article was Successfully Created"
+    redirect_to article_path(@article)
+  else
+    render :new
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
 end
   
   private
