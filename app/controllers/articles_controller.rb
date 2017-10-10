9b64f13d66303cb1b11f7d06308a957ec011d7b7
Martin Porter <Admin@shenobis.co.uk>
2017-10-10 17:50:45 +0000

    update show edit Listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,26 @@
 class ArticlesController < ApplicationController
   
+def index
+  @articles = Article.all
+end
+
 def new
   @article = Article.new
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "Article Was Successfully Updated"
+    redirect_to article_path(@article)
+  else
+    render :edit
+  end
+  
 end
 
 def create
@@ -13,12 +32,13 @@ def create
   else
     render :new
   end
-  
-  def show
-    @article = Article.find(params[:id])
-  end
-  
 end
+  
+def show
+  @article = Article.find(params[:id])
+end
+  
+
   
   private
   def article_params
